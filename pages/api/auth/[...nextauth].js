09286c3b6c9e98cb6246c1ebd77db25e18ca6efc
joshwaiam <joshwaiam@gmail.com>
2021-02-09 13:39:59 -0500

    Removed session prop from nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,9 +8,6 @@ const options = {
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
-  session: {
-    jwt: true,
-  },
 }
 
 export default (req, res) => NextAuth(req, res, options)
