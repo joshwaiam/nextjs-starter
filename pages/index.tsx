cce974b1838b8b652c067a2f6d7a443666d4df7a
joshwaiam <joshwaiam@gmail.com>
2021-02-10 20:11:58 -0500

    Added dark color variants

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,13 +15,17 @@ export const Home = (): JSX.Element => {
         Some content
       </ModalWrapper>
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 divide-y">
-        <h1 className="text-3xl font-bold">Component Showcase</h1>
+        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-300">
+          Component Showcase
+        </h1>
         <div className="py-5">
-          <h2 className="text-xl">Dark Mode Toggler</h2>
+          <h2 className="text-xl text-gray-800 dark:text-gray-300">
+            Dark Mode Toggler
+          </h2>
           <DarkModeToggler />
         </div>
         <div className="py-5">
-          <h2 className="text-xl">Modal</h2>
+          <h2 className="text-xl text-gray-800 dark:text-gray-300">Modal</h2>
           <button
             className="bg-primary-700 p-2 rounded text-white"
             onClick={() => setShowModal(true)}
