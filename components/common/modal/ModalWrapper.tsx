34bf38822a21507986e227d47b09d239991b3d83
joshwaiam <joshwaiam@gmail.com>
2021-02-10 21:46:37 -0500

    Fixed issue with ModalWrapper click detection

--- components/common/modal/ModalWrapper.tsx
+++ components/common/modal/ModalWrapper.tsx
@@ -29,34 +29,28 @@ export const ModalWrapper = ({
         leave="ease-in duration-200"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
-        onClick={onClose}
         className="fixed inset-0 transition-opacity"
         aria-hidden="true"
       >
-        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-      </Transition>
+        <div
+          className="absolute inset-0 bg-gray-500 opacity-75"
+          onClick={onClose}
+        ></div>
+        {/* <!-- This element is to trick the browser into centering the modal contents. --> */}
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
 
-      {/* <!-- This element is to trick the browser into centering the modal contents. --> */}
-      <span
-        className="hidden sm:inline-block sm:align-middle sm:h-screen"
-        aria-hidden="true"
-      >
-        &#8203;
-      </span>
-
-      <Transition
-        show={isOpen}
-        enter="ease-out duration-300"
-        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        enterTo="opacity-100 translate-y-0 sm:scale-100"
-        leave="ease-in duration-200"
-        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
-        role="dialog"
-        aria-modal="true"
-      >
-        <div>{children}</div>
+        <div
+          className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div>{children}</div>
+        </div>
       </Transition>
     </div>
   </div>
