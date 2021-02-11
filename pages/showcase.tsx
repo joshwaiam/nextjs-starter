579134ecf0b51f8ace5bc92e5a58d6276049f1fe
joshwaiam <joshwaiam@gmail.com>
2021-02-10 22:01:45 -0500

    Added Loader to the showcase

--- pages/showcase.tsx
+++ pages/showcase.tsx
@@ -1,10 +1,12 @@
 import DarkModeToggler from 'components/common/dark-mode-toggler'
+import Loader from 'components/common/loader'
 import ModalWrapper from 'components/common/modal/ModalWrapper'
 import Head from 'next/head'
 import { useState } from 'react'
 
 export const Home = (): JSX.Element => {
   const [showModal, setShowModal] = useState(false)
+  const [loaderVariant, setLoaderVariant] = useState('default')
 
   return (
     <>
@@ -15,23 +17,48 @@ export const Home = (): JSX.Element => {
         Some content
       </ModalWrapper>
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 divide-y">
-        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-300">
+        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-300 py-5">
           Component Showcase
         </h1>
         <div className="py-5">
-          <h2 className="text-xl text-gray-800 dark:text-gray-300">
+          <h2 className="text-xl text-gray-800 dark:text-gray-300 pb-3">
             Dark Mode Toggler
           </h2>
           <DarkModeToggler />
         </div>
         <div className="py-5">
-          <h2 className="text-xl text-gray-800 dark:text-gray-300">Modal</h2>
+          <h2 className="text-xl text-gray-800 dark:text-gray-300 pb-3">
+            Modal
+          </h2>
           <button
             className="bg-primary-700 p-2 rounded text-white"
             onClick={() => setShowModal(true)}
           >
             Show Modal
           </button>
+        </div>
+        <div className="py-5">
+          <h2 className="text-xl text-gray-800 dark:text-gray-300 pb-3">
+            Loader
+          </h2>
+          <select
+            className="mb-3"
+            onChange={(e) => setLoaderVariant(e.target.value)}
+          >
+            <option>default</option>
+            <option>dual-ring</option>
+            <option>ellipsis</option>
+            <option>facebook</option>
+            <option>grid</option>
+            <option>heart</option>
+            <option>ring</option>
+            <option>ripple</option>
+            <option>roller</option>
+            <option>custom</option>
+          </select>
+          <Loader variant={loaderVariant}>
+            <h3>Custom Loader!</h3>
+          </Loader>
         </div>
       </div>
     </>
