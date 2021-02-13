b2ce0aa22b1567d961ac631c81102586756ea15f
joshwaiam <joshwaiam@gmail.com>
2021-02-13 13:20:20 -0500

    Added mongodb next-auth integration

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 const options = {
+  database: process.env.MONGO_URL,
   providers: [
     Providers.Discord({
       clientId: process.env.DISCORD_CLIENT_ID,
