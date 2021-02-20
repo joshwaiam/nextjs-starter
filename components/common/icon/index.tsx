ccdb5c054fab726fc568699a22411f17003c4d7b
joshwaiam <joshwaiam@gmail.com>
2021-02-19 23:50:01 -0500

    Added default export of all icons

---
+++ components/common/icon/index.tsx
@@ -0,0 +1,19 @@
+import CheckCircleIcon from './CheckCircleIcon'
+import InformationIcon from './InformationIcon'
+import MenuIcon from './MenuIcon'
+import MoonIcon from './MoonIcon'
+import SunIcon from './SunIcon'
+import WarningIcon from './WarningIcon'
+import XCircleIcon from './XCircleIcon'
+import XIcon from './XIcon'
+
+export default {
+  CheckCircleIcon,
+  InformationIcon,
+  MenuIcon,
+  MoonIcon,
+  SunIcon,
+  WarningIcon,
+  XCircleIcon,
+  XIcon,
+}
